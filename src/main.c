e2cf149b88cd2caf07b640b265f923f6d5f4f599
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-08-23 14:53:55 -0300

    style: Use formatter in main.c.

--- src/main.c
+++ src/main.c
@@ -1,41 +1,48 @@
-#include "semaphore/semaphore.h"
 #include "listener/listener.h"
+#include "semaphore/semaphore.h"
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
 semaphore *sem;
 
-void* wait_for_key() {
-    // Wait for the 'q' key to be pressed
-    semaphore_wait(sem);
-    listener_wait('q');
-    printw("Key 'q' pressed!\n");
-    semaphore_signal(sem);
-    return NULL;
-}
+void *wait_for_key();
 
 int main() {
-    listener_init();
-    sem = malloc(sizeof(semaphore));
-    semaphore_init(sem, 1);
+  listener_init();
+  sem = malloc(sizeof(semaphore));
+  semaphore_init(sem, 1);
 
-    pthread_t thread;
-    pthread_create(&thread, NULL, wait_for_key, NULL);
+  pthread_t thread;
+  pthread_create(&thread, NULL, wait_for_key, NULL);
 
-    printw("Waiting for 'q' key press...\n"); // Use printw while the listener is active
-    refresh(); // Call refresh after printw (apparently it's to apply the changes to the screen but it seems to work without it , but the example I found use it)
+  printw("Waiting for 'q' key press...\n"); // Use printw while the listener is
+                                            // active
+  refresh(); // Call refresh after printw (apparently it's to apply the changes
+             // to the screen but it seems to work without it , but the example
+             // I found use it)
 
-    semaphore_wait(sem);
+  semaphore_wait(sem);
 
-    printw("Exiting listener and semaphore.\n"); // This message should never be seen because the listener is holding the semaphore
-    refresh();
-    pthread_join(thread, NULL);
+  printw("Exiting listener and semaphore.\n"); // This message should never be
+                                               // seen because the listener is
+                                               // holding the semaphore
+  refresh();
+  pthread_join(thread, NULL);
 
-    semaphore_signal(sem);
+  semaphore_signal(sem);
 
-    semaphore_destroy(sem);
-    listener_destroy();
-    printf("Exiting program.\n");
-    return 0;
+  semaphore_destroy(sem);
+  listener_destroy();
+  printf("Exiting program.\n");
+  return 0;
+}
+
+void *wait_for_key() {
+  // Wait for the 'q' key to be pressed
+  semaphore_wait(sem);
+  listener_wait('q');
+  printw("Key 'q' pressed!\n");
+  semaphore_signal(sem);
+  return NULL;
 }
