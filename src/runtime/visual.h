2ad5321c07b46af4638c5a8ca75ac362d82d0392
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-08-24 23:32:03 -0300

    docs: Add doxygen to visual.h.

--- src/runtime/visual.h
+++ src/runtime/visual.h
@@ -8,7 +8,27 @@
 const int MENU_HEIGHT = 3;
 const int MENU_WIDTH = 60;
 
+/*
+ * @brief Create a virtual window.
+ * * Create a virtual window with ncurses.
+ * @param start_y Start position in y axle.
+ * @param start_x Start position in x axle.
+ * @param height Height of the window.
+ * @param windth Width of the window.
+ * @return Return a new window.
+ */
 WINDOW *create_window(int start_y, int start_x, int height, int width);
+
+/*
+ * @brief Create a virtual window.
+ * * Create a virtual window with ncurses.
+ * @param text Text to show in window.
+ * @param start_y Start position in y axle.
+ * @param start_x Start position in x axle.
+ * @param height Height of the window.
+ * @param windth Width of the window.
+ * @return Return a new window.
+ */
 WINDOW *create_text_window(char *text, int start_y, int start_x, int height,
                            int width);
 
