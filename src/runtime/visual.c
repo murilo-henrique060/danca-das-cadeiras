800b7223f3b2eb2a088815ebe3263d69f62df557
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-08-24 23:25:03 -0300

    feat: Create window creations to use titles.

--- src/runtime/visual.c
+++ src/runtime/visual.c
@@ -1,20 +1,34 @@
 #include "visual.h"
 
 #include <ncurses.h>
+#include <string.h>
+#include <unistd.h>
 
 WINDOW *create_window(int start_y, int start_x, int height, int width) {
   WINDOW *local_window;
 
   local_window = newwin(height, width, start_y, start_x);
-  box(local_window, 0, 0);
-  wrefresh(local_window);
+
+  return local_window;
+}
+
+WINDOW *create_text_window(char *text, int start_y, int start_x, int height,
+                           int width) {
+  // calcule to start text
+  const int start_text_x = (width - strlen(text)) / 2;
+  WINDOW *local_window = create_window(start_y, start_x, height, width);
+
+  // window borders
+  wborder(local_window, '|', '|', '-', '-', '-', '-', '-', '-');
+
+  // show the text
+  mvwprintw(local_window, (int)(height / 2), start_text_x, "%s", text);
 
   return local_window;
 }
 
 void print_menu() {
-  WINDOW *local_window;
-  local_window = create_window(0, 0, MENU_HEIGHT, MENU_WIDTH);
-
-  printw("Dança das Cadeiras");
+  WINDOW *local_window =
+      create_text_window(GAME_NAME, 0, 0, MENU_HEIGHT, MENU_WIDTH);
+  wrefresh(local_window);
 }
--- src/runtime/visual.h
+++ src/runtime/visual.h
@@ -3,11 +3,14 @@
 
 #include <ncurses.h>
 
+#define GAME_NAME "Dança das Cadeiras"
+
 const int MENU_HEIGHT = 3;
 const int MENU_WIDTH = 60;
 
 WINDOW *create_window(int start_y, int start_x, int height, int width);
-WINDOW *create_title_window(int start_y, int start_x, int height, int width);
+WINDOW *create_text_window(char *text, int start_y, int start_x, int height,
+                           int width);
 
 /*
  * @brief Show the main menu.
