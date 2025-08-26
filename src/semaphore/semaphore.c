4968ff3e53242c9fcdf22e27569a1a2c97c10da8
murilo-henrique060 <henrique.henryconde@gmail.com>
2025-08-26 17:34:45 -0300

    feat: Add documentation comments for semaphore functions

--- src/semaphore/semaphore.c
+++ src/semaphore/semaphore.c
@@ -1,12 +1,26 @@
 #include "semaphore.h"
 #include <pthread.h>
 
+/**
+ * @brief Initialize the semaphore.
+ * 
+ * @param s Pointer to the semaphore.
+ * @param initial_count Initial count for the semaphore.
+ */
 void semaphore_init(semaphore* s, int initial_count) {
     s->count = initial_count;
     pthread_mutex_init(&s->mutex, NULL);
     pthread_cond_init(&s->cond, NULL);
 }
 
+/**
+ * @brief Wait for the semaphore.
+ * 
+ * This function waits for the semaphore to become available.
+ * If another thread is holding the semaphore, it will be blocked until the first one access the semaphore.
+ * 
+ * @param s Pointer to the semaphore.
+ */
 void semaphore_wait(semaphore* s) {
     pthread_mutex_lock(&s->mutex);
     while (s->count <= 0) {
@@ -16,6 +30,14 @@ void semaphore_wait(semaphore* s) {
     pthread_mutex_unlock(&s->mutex);
 }
 
+
+/**
+ * @brief Releases the semaphore that is being held.
+ * 
+ * This function signals the semaphore, allowing other threads to access it.
+ * 
+ * @param s Pointer to the semaphore.
+ */
 void semaphore_signal(semaphore* s) {
     pthread_mutex_lock(&s->mutex);
     s->count++;
@@ -23,6 +45,13 @@ void semaphore_signal(semaphore* s) {
     pthread_mutex_unlock(&s->mutex);
 }
 
+/**
+ * @brief Destroys the semaphore.
+ * 
+ * This function cleans up the resources used by the semaphore.
+ * 
+ * @param s Pointer to the semaphore.
+ */
 void semaphore_destroy(semaphore* s) {
     pthread_mutex_destroy(&s->mutex);
     pthread_cond_destroy(&s->cond);
