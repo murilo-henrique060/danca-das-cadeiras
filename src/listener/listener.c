5218ca887b66cdf5e1c874518f286234d940c360
murilo-henrique060 <henrique.henryconde@gmail.com>
2025-08-29 12:24:03 -0300

    refactor: Simplify listener structure and improve key subscription logic

--- src/listener/listener.c
+++ src/listener/listener.c
@@ -18,7 +18,6 @@ typedef struct {
     pthread_cond_t cond;
     
     unsigned int size;
-    unsigned int subscribed;
     ListenerKey** keys;
 } Listener;
 
@@ -30,20 +29,7 @@ void* listener_listen(void* arg) {
     pthread_cond_signal(&listener->cond);
 
     while (true) {
-        printw("listener->keys: ");
-        for (int k = 0; k < listener->size; k++) {
-            if (listener->keys[k] != NULL) {
-            printw("%c ", listener->keys[k]->key);
-            } else {
-            printw("_ ");
-            }
-        }
-        printw("\n");
-        refresh();
-
         ch = getch();
-        printw("Key pressed: %c\n", ch);
-        refresh();
 
         pthread_mutex_lock(&listener->mutex);
         for (i = 0; i < listener->size; i++) {
@@ -59,7 +45,7 @@ void* listener_listen(void* arg) {
         }
         pthread_mutex_unlock(&listener->mutex);
         
-        while (listener->subscribed == 0) {
+        while (listener->size == 0) {
             pthread_cond_wait(&listener->cond, &listener->mutex);
         }
 
@@ -67,21 +53,17 @@ void* listener_listen(void* arg) {
     }
 }
 
-Listener* listener_init(unsigned int size) {
+Listener* listener_init() {
     initscr(); // Initialize ncurses
     cbreak(); // Disable line buffering
     noecho(); // Disable echoing of typed characters
-    // nodelay(stdscr, TRUE); // Don't block on getch()
     keypad(stdscr, TRUE); // Enable function keys
     
     Listener* listener = malloc(sizeof(Listener));
-    listener->size = size;
-    listener->subscribed = 0;
-    listener->keys = calloc(sizeof(ListenerKey*), size);
+    listener->size = 0;
+    listener->keys = NULL;
     pthread_mutex_init(&listener->mutex, NULL);
     pthread_cond_init(&listener->cond, NULL);
-
-    refresh();
 
     pthread_create(&listener->thread, NULL, listener_listen, listener);
 
@@ -99,6 +81,7 @@ void listener_destroy(Listener* listener) {
     for (int i = 0; i < listener->size; i++) {
         listenerKey = listener->keys[i];
         if (listenerKey == NULL) continue;
+
         pthread_mutex_destroy(&listenerKey->mutex);
         pthread_cond_destroy(&listenerKey->cond);
         free(listenerKey);
@@ -114,7 +97,7 @@ void listener_destroy(Listener* listener) {
     free(listener);
 }
 
-unsigned int listener_subscribe(Listener* listener, char key) {
+ListenerKey* listener_subscribe(Listener* listener, char key) {
     pthread_mutex_lock(&listener->mutex);
 
     ListenerKey* listenerKey = malloc(sizeof(ListenerKey));
@@ -123,88 +106,51 @@ unsigned int listener_subscribe(Listener* listener, char key) {
     listenerKey->key = key;
     listenerKey->status = 0;
 
-    int i;
-    for (i = 0; i < listener->size; i++) {
-        if (listener->keys[i] == NULL) {
-            listener->keys[i] = listenerKey;
-            listener->subscribed++;
+    listener->keys = realloc(listener->keys, sizeof(ListenerKey*) * (listener->size + 1));
+    listener->keys[listener->size] = listenerKey;
+    listener->size++;
+
+    pthread_cond_signal(&listener->cond);
+    pthread_mutex_unlock(&listener->mutex);
+
+    return listenerKey;
+}
+
+void listener_unsubscribe(Listener* listener, ListenerKey* listenerKey) {
+    pthread_mutex_lock(&listener->mutex);
+
+    unsigned int listenerKeyId = 0;
+    for (int i = 0; i < listener->size; i++) {
+        listenerKeyId = i;
+        if (listener->keys[i] == listenerKey) {
             break;
         }
     }
     
-    pthread_cond_signal(&listener->cond);
-    pthread_mutex_unlock(&listener->mutex);
-
-    return i;
-}
-
-void listener_unsubscribe(Listener* listener, unsigned int listenerKeyId) {
-    pthread_mutex_lock(&listener->mutex);
-
-    ListenerKey* listenerKey = listener->keys[listenerKeyId];
     pthread_mutex_destroy(&listenerKey->mutex);
     pthread_cond_destroy(&listenerKey->cond);
     free(listenerKey);
 
-    listener->keys[listenerKeyId] = NULL;
-    listener->subscribed--;
+    listener->size--;
+    listener->keys[listenerKeyId] = listener->keys[listener->size];
+    listener->keys = realloc(listener->keys, sizeof(ListenerKey*) * listener->size);
 
     pthread_mutex_unlock(&listener->mutex);
 }
 
 void listener_wait(Listener* listener, char key) {
-    unsigned int listenerKeyId = listener_subscribe(listener, key);
+    ListenerKey* listenerKey = listener_subscribe(listener, key);
     
-    ListenerKey* listenerKey = listener->keys[listenerKeyId];
     pthread_mutex_lock(&listenerKey->mutex);
     while (!listenerKey->status) {
         pthread_cond_wait(&listenerKey->cond, &listenerKey->mutex);
     }
     pthread_mutex_unlock(&listenerKey->mutex);
     
-    listener_unsubscribe(listener, listenerKeyId);
+    listener_unsubscribe(listener, listenerKey);
 }
 
 typedef struct {
     Listener* listener;
     char key;
 } WaitForKeyArgs;
-
-void *wait_for_key(void *args)  {
-    WaitForKeyArgs* waitArgs = (WaitForKeyArgs*)args;
-    Listener* listener = waitArgs->listener;
-    char key = waitArgs->key;
-
-    printw("Waiting for key press '%c'...\n", key);
-    refresh();
-    listener_wait(listener, key);
-    printw("Key '%c' pressed!\n", key);
-    refresh();
-    pthread_exit(NULL);
-}
-
-int main() {
-    unsigned int size = 5;
-    
-    Listener* listener = listener_init(size);
-    char keys[] = {'a', 'd', 'g', 'j', 'l'};
-    pthread_t threads[size];
-
-    printw("Pressione a tecla 'a', 'd', 'g', 'j', ou 'l' para continuar...\n");
-    refresh();
-
-    for (int i = 0; i < size; i++) {
-        WaitForKeyArgs* args = malloc(sizeof(WaitForKeyArgs));
-        args->listener = listener;
-        args->key = keys[i];
-        pthread_create(&threads[i], NULL, wait_for_key, args);
-    }
-
-    for (int i = 0; i < size; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
-    listener_destroy(listener);
-    printf("Programa finalizado!\n");
-    return 0;
-}
--- src/listener/listener.h
+++ src/listener/listener.h
@@ -15,9 +15,38 @@
 #define LISTENER_H
 
 /**
- * @brief The listener structure
+ * @brief The waited key structure
+ * @details The waited key structure contains the key to be listened to and its status.
  */
-typedef Listener;
+typedef struct {
+    char key; ///< The key to be listened to
+    unsigned int status; ///< The status of the key (0 = not pressed, 1 = pressed)
+    pthread_mutex_t mutex; ///< Mutex to protect access to the key status
+    pthread_cond_t cond; ///< Condition variable to wait for the key press
+} ListenerKey;
+
+/**
+ * @brief The listener structure
+ * @details The listener structure contains the main listener thread and a list of keys to be listened to.
+ *          It also contains synchronization primitives to protect access to the key list.
+ *          The key list is dynamically allocated and can grow as needed.
+ */
+typedef struct {
+    pthread_t thread; ///< The main listener thread
+
+    pthread_mutex_t mutex; ///< Mutex to protect access to the listener
+    pthread_cond_t cond; ///< Condition variable to wait for key events
+
+    unsigned int size; ///< The number of keys being listened to
+    ListenerKey** keys; ///< The list of keys being listened to
+} Listener;
+
+/**
+ * @brief The main listener thread function
+ * @details This function listens for key events and notifies the corresponding listener keys.
+ * @param arg Pointer to the listener structure
+ */
+void* listener_listen(void* arg);
 
 /**
  * @brief Initialize the ncurse library, create a listener and start the listener thread
@@ -29,5 +58,30 @@ Listener* listener_init();
  * @brief Destroy the listener and free its resources
  */
 void listener_destroy(Listener* listener);
+
+/**
+ * @brief Subscribe a key to the listener
+ * @details This function creates a new listener key and adds it to the listener's key list.
+ * @param listener Pointer to the listener structure
+ * @param key The key to be subscribed
+ * @return A pointer to the created listener key
+ */
+ListenerKey* listener_subscribe(Listener* listener, char key);
+
+/**
+ * @brief Unsubscribe a key from the listener
+ * @details This function removes a listener key from the listener's key list and frees its resources.
+ * @param listener Pointer to the listener structure
+ * @param listenerKey Pointer to the listener key to be unsubscribed
+ */
+void listener_unsubscribe(Listener* listener, ListenerKey* listenerKey);
+
+/**
+ * @brief Wait for a key to be pressed
+ * @details This function subscribes a key to the listener and waits for it to be pressed.
+ * @param listener Pointer to the listener structure
+ * @param key The key to be waited for
+ */
+void listener_wait(Listener* listener, char key);
 
 #endif // LISTENER_H
